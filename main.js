f0765dbee5cdf3c5b052335c3125653ec52bd5aa
Darya <daraziroh@gmail.com>
2020-01-27 14:26:44 +0300

    servicespage added

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
 
-  var searchButton = $('#searchButton');
-  var searchField = $('#searchField')
-  searchButton.on('click', function (e) {
-    e.preventDefault()
-    console.log("1")
-    searchField.toggleClass("searchFieldHidden");
-    console.log("2")
-  });
+	var searchButton = $('#searchButton');
+	var searchField = $('#searchField')
+	searchButton.on('click', function (e) {
+		e.preventDefault()
+		searchField.toggleClass("searchFieldHidden");
+	});
 
+	var serviceItem = $(".services-item");
+	var serviceItemTitle = $(".services-item-title");
+	var serviceItemText = $(".service-item-text");
+	var serviceItemButton = $(".service-item-button");
 
+	serviceItem.on( "mouseenter mouseleave", function(e) {
+		console.log("1");
+		e.preventDefault()
+		$(this).toggleClass("change-services-item");
+	});
 });
-
-
 
 
